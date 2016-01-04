d310dd7c36486d62fc6834889a21f941eeeb6497
Teddy Otieno <teddy.asola@andela.com>
2016-01-04 15:35:17 +0300

    [Fixes #110883222] Include login function on the scope object

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,9 +1,22 @@
 angular.module('dms.controllers')
-    .controller('HeaderCtrl', ['$scope', function($scope) {
-        // $scope.openMenu = function($mdOpenMenu, ev) {
-        //     var originatorEv;
-        //     originatorEv = ev;
-        //     $mdOpenMenu(ev);
-        // };
+    .controller('HeaderCtrl', ['$scope', '$rootScope', '$state', '$window', 'Users', 'Auth', function($rootScope, $scope, $state, $window, Users, Auth) {
+        $scope.user = {};
+        $scope.login = function() {
+            Users.login($scope.user, function(err, res) {
+                if (!err) {
+                    Auth.setToken(res.token);
+                    var user = {};
+                    user.name = res.firstname + ' ' + res.lastname;
+                    $rootScope.currentUser = user;
+                    $state.go('documents');
+                } else {
+                    $scope.messageLogin = err.error ||
+                    err.message || err[0].message;
+                    console.log($scope.messageLogin);
+                }
+            });
+        };
+        console.log($scope.users);
+
 
     }]);
