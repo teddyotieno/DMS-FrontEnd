b4c6d5fe3edf1d75d4aa9f04134c1a0062ca1265
Teddy Otieno <teddy.asola@andela.com>
2016-01-04 10:26:04 +0300

    [Fixes #110883222] Initial configuration for Header controller

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,9 @@
+angular.module('dms.controllers')
+    .controller('HeaderCtrl', ['$scope', function($scope) {
+        // $scope.openMenu = function($mdOpenMenu, ev) {
+        //     var originatorEv;
+        //     originatorEv = ev;
+        //     $mdOpenMenu(ev);
+        // };
+
+    }]);
