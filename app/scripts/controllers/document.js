20708952766bc694e4b889f59f5ea6fe41d16162
Teddy Otieno <teddy.asola@andela.com>
2016-01-06 17:50:33 +0300

    [Fixes #110883222] $openOffScreen function for creating new document

--- app/scripts/controllers/document.js
+++ app/scripts/controllers/document.js
@@ -1,6 +1,32 @@
 angular.module('dms.controllers')
-  .controller('DocumentsCtrl', ['$scope', 'Documents', function($scope, Documents) {
-    $scope.documents = Documents.query();
-    console.log($scope.documents);
+    .controller('DocumentsCtrl', ['$scope', 'Documents',  '$mdDialog', 'Users', function($scope, Documents, $mdDialog) {
+        $scope.documents = Documents.query();
 
-  }]);
+        $scope.openOffscreen = function(ev) {
+            $mdDialog.show({
+                    controller: DialogController,
+                    templateUrl: '../views/form.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true
+                })
+                .then(function(answer) {
+                    $scope.status = 'You said the information was "' + answer + '".';
+                }, function() {
+                    $scope.status = 'You cancelled the dialog.';
+                });
+        };
+
+        function DialogController($scope, $mdDialog) {
+            $scope.hide = function() {
+                $mdDialog.hide();
+            };
+            $scope.cancel = function() {
+                $mdDialog.cancel();
+            };
+            $scope.answer = function(answer) {
+                $mdDialog.hide(answer);
+            };
+        }
+
+    }]);
