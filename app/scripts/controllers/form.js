b026baddb0ef6e89018436456e7092251252aa07
Teddy Otieno <teddy.asola@andela.com>
2016-01-11 15:42:21 +0300

    [Fixes #110883210] Refactor form controller

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -1,12 +1,13 @@
 angular.module('dms.controllers')
-    .controller('FormCtrl', ['$scope', 'Documents', '$mdDialog', '$mdToast',  function($scope, Documents, $mdDialog, $mdToast) {
+    .controller('FormCtrl', ['$scope', 'Documents', '$mdDialog', '$mdToast', '$rootScope', 'Users', function($scope, Documents, $mdDialog, $mdToast, $rootScope, Users) {
         $scope.createDoc = function() {
             if ($scope.document.title && $scope.document.content) {
                 Documents.save($scope.document, function(res) {});
                 $mdToast.show($mdToast.simple().textContent('Document Created').hideDelay(3000));
                 $mdDialog.hide();
+                $scope.userDocuments();
             } else {
-                $scope.message = 'Document need to be defind';
+                $scope.message = 'Document need to be defined';
             }
         };
     }]);
