fe5b1a618b77c4a020dcab136bee5d5adfd529d2
Teddy Otieno <teddy.asola@andela.com>
2016-01-15 16:10:44 +0300

    [Fixes #110883210] Setup parallax and typewritter function and bind them on welcome scope object

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -1,4 +1,21 @@
 angular.module('dms.controllers')
-  .controller('WelcomeCtrl', ['$scope', function($scope){
-    $scope.message = 'Welcome to the Home Page of our app';
-  }]);
+    .controller('WelcomeCtrl', ['$scope', function($scope) {
+        $('.header-content').mousemove(function(event) {
+            var containerWidth = $(this).innerWidth(),
+                containerHeight = $(this).innerHeight(),
+                mousePositionX = (event.pageX / containerWidth) * 10,
+                mousePositionY = (event.pageY / containerHeight) * 10;
+
+            $(this).css('background-position', mousePositionX + '%' + ' ' + mousePositionY + '%');
+        });
+
+        $(function(){
+        $('.dynamic-text-right').typed({
+            strings: ['Create^2000', 'Manage^1000', 'Organize^1000'],
+            typeSpeed: 0,
+            loop: true,
+            //backDelay: 500
+        });
+    });
+    }]);
+
