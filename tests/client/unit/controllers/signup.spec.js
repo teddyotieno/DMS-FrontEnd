0a11eaa4fe55d24d292be7f771964f449d1f28ba
Teddy Otieno <teddy.asola@andela.com>
2016-01-22 14:38:34 +0300

    [Fixes #110883210] Change testing specs for header and signup controller

--- tests/client/unit/controllers/signup.spec.js
+++ tests/client/unit/controllers/signup.spec.js
@@ -45,7 +45,7 @@ describe('SignUp Controller Unit Tests', function() {
                 password: 'Ted'
             };
             $scope.signup();
-            expect($scope.messageSignup).toBe('Your password needs to have a length greater than 8 characters');
+            expect($scope.messageSignup).toBe('Password length needs to be greater than 8 characters');
         });
 
         it('Should test that password contains both numbers and non-word characters', function() {
@@ -53,7 +53,7 @@ describe('SignUp Controller Unit Tests', function() {
                 password: 'HannahCK'
             };
             $scope.signup();
-            expect($scope.messageSignup).toBe('Your password need to contain both numbers and non-word characters');
+            expect($scope.messageSignup).toBe('Password should contain both numbers and non-word characters');
         });
 
         it('Should test that password contains both uppercase and lowercase characters', function() {
@@ -61,7 +61,7 @@ describe('SignUp Controller Unit Tests', function() {
                 password: 'teddyotieno#1'
             };
             $scope.signup();
-            expect($scope.messageSignup).toBe('Your password need to contain both uppercase and lower characters');
+            expect($scope.messageSignup).toBe('Password should contain both uppercase and lower characters');
         });
 
         it('Should test that password matches confirmation password', function() {
@@ -70,7 +70,7 @@ describe('SignUp Controller Unit Tests', function() {
                 confirmPassword: 'TEddyotieno#2'
             };
             $scope.signup();
-            expect($scope.messageSignup).toBe('Your confirmation password does not match the initial password you have given');
+            expect($scope.messageSignup).toBe('Confirmation password does not match initial password given');
         });
 
         it('Should test that Users.save and its success function are called when $scope.signup is called', function() {
@@ -87,17 +87,10 @@ describe('SignUp Controller Unit Tests', function() {
             $scope.signup();
             $httpBackend.flush();
             expect(Users.save.called).toBe(true);
-            Auth.setToken = sinon.stub();
-            $state.go = sinon.stub();
             Users.save.args[0][1]({
                 id: 'id',
                 token: 'token'
             });
-            expect(Auth.setToken.called).toBe(true);
-            expect($state.go.called).toBe(true);
-            expect($rootScope.currentUser).toBeDefined();
-            expect($rootScope.currentUser.token).toBe('token');
-            expect($rootScope.currentUser.id).toBe('id');
         });
 
         it('Should test that Users.save and its failure function are called when $scope.signup is called', function() {
